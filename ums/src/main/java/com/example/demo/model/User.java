cb7760143d246e8a38de3361bc2e6c5f8ed2e80f
RAHMANI <NDH00592@Nmcorp.Nissan.Biz>
2019-10-31 10:39:13 +0530

    fourth commit

--- ums/src/main/java/com/example/demo/model/User.java
+++ ums/src/main/java/com/example/demo/model/User.java
@@ -17,7 +17,6 @@ public class User implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	@Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long user_id;
 	private String username;
 	private String email;
