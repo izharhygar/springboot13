d95dd83070e45883b5a4afec82d097a3de8fa597
RAHMANI <NDH00592@Nmcorp.Nissan.Biz>
2019-11-01 13:51:06 +0530

    sixth commit ( added unique - 2 )

--- ums/src/main/java/com/example/demo/controller/UserController.java
+++ ums/src/main/java/com/example/demo/controller/UserController.java
@@ -2,7 +2,11 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import javax.xml.ws.http.HTTPBinding;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,10 +37,19 @@ public class UserController {
 //		return this.userRepo.findOne(Id);
 //	}
 	@PostMapping(produces = "application/json")
-	public List<User> putoneuser(@RequestBody User user)
-	{
-		this.userRepo.save(user);
-		return userRepo.findAll();
+	public 	ResponseEntity<User> putoneuser(@RequestBody User user)
+	{   long old_count = this.userRepo.count();
+	    if(this.userRepo.existsById(user.getUser_id()))
+	    	return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
+	    else {this.userRepo.save(user);
+		long new_count = this.userRepo.count();
+		System.out.println(old_count+"  .."+new_count);
+		if(new_count>old_count)
+			return new ResponseEntity<User>(HttpStatus.OK);
+		else
+			return new ResponseEntity<User>(HttpStatus.BAD_REQUEST);
+	    }
+			 
 	}
 	
 	@PutMapping(value="/{id}",produces = "application/json")
